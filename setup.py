a02c5cc376f8cccd3389072c03d0a0e538a0b979
nvamshik <30487695+nvamshik@users.noreply.github.com>
2017-07-27 18:14:04 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cloud_util_s3',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/nvamshik/cloud_util_s3',
+    license='',
+    author='Vamshi',
+    author_email='vnallamothu@quadyster.com',
+    description='Python module to upload an object/file into AWS S3.'
+)
