853645a560397ac27bff1d58f1d1e4ea29cf99b5
nvamshik <30487695+nvamshik@users.noreply.github.com>
2017-07-27 20:39:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
+with open('README.rst') as f:
+    readme = f.read()
 setup(
     name='cloud_util_s3',
     version='0.0.1',
-    packages=[''],
-    url='https://github.com/nvamshik/cloud_util_s3',
-    license='',
-    author='Vamshi',
+    description='Python module to upload an object/file into AWS S3.',
+    long_description=readme,
+    author='Quadyster_dev_team',
     author_email='vnallamothu@quadyster.com',
-    description='Python module to upload an object/file into AWS S3.'
+    url='https://github.com/nvamshik/cloud_util_s3',
+    packages=find_packages(exclude=('tests', 'docs'))
 )
