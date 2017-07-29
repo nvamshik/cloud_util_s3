a09c0b3881eaf1fcb619950049b914f1c3488395
nvamshik <vnallamothu@quadyster.com>
2017-07-28 19:57:37 -0500

    Coded for uploading an object into s3.

--- cloud_util_s3.py
+++ cloud_util_s3.py
@@ -1,8 +1,22 @@
 import boto3
 
-s3_resource = boto3.resource('s3')
-for bucket in s3_resource.buckets.all():
-    print(bucket.name)
+def main():
+    s3_upload("C:/Users/vamshi/Desktop/Vamshi_WordPress.txt")
 
-with open('filename', 'rb') as data:
-    s3_resource.upload_fileobj(data, 'mycalibucket', 'mykey')
+def s3_upload(template_file):
+    with open(template_file) as f:
+        template = f.read()
+
+def s3_bucket(bucket_name):
+    with open(bucket_name) as v:
+        bucket = v.read()
+
+def s3_file(file_name):
+    with open(file_name) as b:
+        uploadfilename = b.read()
+
+    s3_client = boto3.client("s3")
+    s3_client.upload_file(template_file, bucket_name, file_name)
+
+if __name__ == "__main__":
+    main()
