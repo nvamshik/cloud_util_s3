cc47b995c34b81e54a548ef6795163aed10472e2
nvamshik <vnallamothu@quadyster.com>
2017-07-31 13:24:25 -0500

    Coded for uploading an object into s3.

--- README.rst
+++
@@ -1,6 +0,0 @@
-# cloud_util_s3
-Creation of s3 upload object Repository
-========================
-This is a simple cloud_util python project which helps us to upload a file/object into AWS S3. 
-This project can be found at: https://github.com/nvamshik/cloud_util_s3
-
--- cloud_util_s3.py
+++ cloud_util_s3.py
@@ -1,14 +1,27 @@
 import boto3
 
 def main():
-    s3_upload("C:/Users/vamshi/Desktop/wordpress.txt")
+
+
+
+ """   s3_upload("C:/Users/vamshi/Desktop/wordpress.txt")
+   s3_bucket("cc-sample-bucket")
+   s3_key("wordpress.txt")
 
 def s3_upload(template_file):
-    with open(template_file) as f:
-        template = f.read()
+    f = open(template_file)
+    template_file = f.read()
 
-    s3_client = boto3.client("s3")
-    s3_client.upload_file(template_file,"cc-sample-bucket","wordpress.txt")
+def s3_bucket(bucket_name):
+    with open(bucket_name) as v:
+        bucket = v.read()
+
+def s3_key(key_name):
+    with open(key_name) as r:
+        bucket = r.read()"""
+
+s3_client = boto3.client("s3")
+s3_client.upload_file("C:/Users/vamshi/Desktop/wordpress.txt", "cc-sample-bucket", "wordpress.txt")
 
 if __name__ == "__main__":
     main()
